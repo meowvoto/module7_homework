b7789f1d57005a2804aae1ee97b7f0b514495278
Victoria Rossiyskaya <meowvoto@gmail.com>
2021-03-12 00:11:34 +0300

    task_1 is ready

--- task_1.js
+++ task_1.js
@@ -1,17 +1,16 @@
 // Написать, функцию, которая принимает в качестве аргумента объект и выводит в консоль все ключи и значения только собственных свойств. Данная функция не должна возвращать значение. 
 
-let mother = {
+const mother = {
     name: "Alina",
     age: 30,
     eyes: "brown",
     hair: "black"
 };
 
-let daughter = {
-    name: "Anna",
-    age: "10",
-    freckles: true
-};
+const daughter = Object.create(mother);
+daughter.name =  "Anna";
+daughter.age = 10;
+daughter.freckles = true;
 
 function showProperties(object) {
     for (let key in object) {
